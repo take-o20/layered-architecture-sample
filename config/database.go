7293fc0a60e8155a67d05525523db7ce94d5d5bc
Takeo Udo <take_o20@icloud.com>
2024-02-26 22:10:11 +0900

    add config

---
+++ config/database.go
@@ -0,0 +1,62 @@
+package config
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+var DB *sql.DB
+
+func Init() {
+	mustGetenv := func(k string) string {
+		v := os.Getenv(k)
+		if v == "" {
+			log.Fatalf("Fatal Error in connect_tcp.go: %s environment variable not set.", k)
+		}
+		return v
+	}
+
+	var (
+		dbUser    = mustGetenv("DB_USER")       // e.g. 'my-db-user'
+		dbPwd     = mustGetenv("DB_PASS")       // e.g. 'my-db-password'
+		dbName    = mustGetenv("DB_NAME")       // e.g. 'my-database'
+		dbPort    = mustGetenv("DB_PORT")       // e.g. '3306'
+		dbTCPHost = mustGetenv("INSTANCE_HOST") // e.g. '127.0.0.1' ('172.17.0.1' if deployed to GAE Flex)
+	)
+
+	dbURI := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?parseTime=true",
+		dbUser, dbPwd, dbTCPHost, dbPort, dbName)
+
+	var dbOpenErr error
+	DB, dbOpenErr = sql.Open("mysql", dbURI)
+	if dbOpenErr != nil {
+		fmt.Fprintf(os.Stderr, "%s\n", dbOpenErr)
+		os.Exit(1)
+	}
+	// エラーハンドリング省略
+
+	if err := DB.Ping(); err != nil {
+		fmt.Fprintf(os.Stderr, "%s\n", err)
+		os.Exit(1)
+	}
+
+	if err := migrateDB(DB); err != nil {
+		fmt.Fprintf(os.Stderr, "%s\n", err)
+		os.Exit(1)
+	}
+}
+
+func migrateDB(db *sql.DB) error {
+	createVotes := `CREATE TABLE IF NOT EXISTS users (
+		id SERIAL NOT NULL,
+		name VARCHAR(255) NOT NULL,
+		email VARCHAR(255) NOT NULL,
+		PRIMARY KEY (id)
+	);`
+	_, err := db.Exec(createVotes)
+	return err
+}
--- go.mod
+++ go.mod
@@ -2,4 +2,11 @@ module github.com/take-o20/layered-architecture-sample
 
 go 1.21.7
 
-require github.com/google/uuid v1.6.0 // indirect
+require (
+	github.com/go-sql-driver/mysql v1.7.1
+	github.com/google/uuid v1.6.0 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
+	github.com/julienschmidt/httprouter v1.3.0 // indirect
+	github.com/nahojer/httprouter v1.0.4 // indirect
+	github.com/nahojer/routes v0.0.2 // indirect
+)
