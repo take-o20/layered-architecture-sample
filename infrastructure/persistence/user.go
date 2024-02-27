97574ea77189763b1d3eff18e8a3ebfb5acbecdf
Takeo Udo <take_o20@icloud.com>
2024-02-27 20:17:31 +0900

    implement user List

--- infrastructure/persistence/user.go
+++ infrastructure/persistence/user.go
@@ -2,6 +2,7 @@ package persistence
 
 import (
 	"database/sql"
+	"fmt"
 
 	"github.com/take-o20/layered-architecture-sample/domain"
 	"github.com/take-o20/layered-architecture-sample/domain/repository"
@@ -28,9 +29,34 @@ func (up userPersistence) GetByUserID(DB *sql.DB, userID string) (*domain.User, 
 	return convertToUser(row)
 }
 
-func (up userPersistence) List(DB *sql.DB) ([]domain.User, error)
+func (up userPersistence) List(DB *sql.DB) ([]domain.User, error) {
+	rows, err := DB.Query("SELECT * FROM users")
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	users, err := convertToUserList(rows)
+	if err != nil {
+		return nil, fmt.Errorf("failed to List users: %w", err)
+	}
+	return users, nil
+}
 func (up userPersistence) Update(DB *sql.DB, userID, name, email string) (*domain.User, error)
 func (up userPersistence) Delete(DB *sql.DB, userID string) (*domain.User, error)
+
+func convertToUserList(rows *sql.Rows) ([]domain.User, error) {
+	users := []domain.User{}
+	for rows.Next() {
+		user := domain.User{}
+		err := rows.Scan(&user.UserID, &user.Name, &user.Email)
+		if err != nil {
+			return nil, fmt.Errorf("failed to List users: %w", err)
+		}
+		users = append(users, user)
+	}
+	return users, nil
+}
 
 func convertToUser(row *sql.Row) (*domain.User, error) {
 	user := domain.User{}
